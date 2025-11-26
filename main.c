927057d2e47ff9a96f0d7de35d7786a4a71ef3c1
Natthapongrmutl <nooknattha.a@gmail.com>
2025-11-26 16:38:01 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int N, i;
+    if (scanf("%d", &N) != 1) {
+        return 1;
+    }
+    if (N <=0){
+        printf("error");
+        return 1;
+    }
+    for (i = 0; i < N; i++) {
+        printf("Hello Loop!\n");
+    }
+    return 0;
+}
