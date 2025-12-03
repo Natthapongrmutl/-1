6654c3513e8f8a53a5719e59f42a82e2cd58ec66
Natthapongrmutl <nooknattha.a@gmail.com>
2025-12-03 13:49:45 +0700

    Update lab1main1.c

--- lab1main1.c
+++ lab1main1.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     int num;
-    if(scanf("%d", &num) != 1){
+
+    printf("ใส่เลข: ");
+    if (scanf("%d", &num) != 1) {
+        printf("Invalid input.\n");
         return 1;
     }
-    for(int i = 1; i <= num; i++){
+
+    if (num <= 0) {
+        printf("ใส่เลขดีๆ\n");
+        return 1;
+    }
+
+    for (int i = 1; i <= num; i++) {
         printf("[%d] Hello world\n", i);
     }
-    
+
     return 0;
 }
