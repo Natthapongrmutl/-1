4473619060ca6f391b545e983d2fb43624692ca9
Natthapongrmutl <nooknattha.a@gmail.com>
2025-12-03 14:33:34 +0700

    Update lab1main1.c

--- lab1main1.c
+++ lab1main1.c
@@ -7,6 +7,10 @@ int main(){
         printf("This is not a number ");
         return 1;
     }
+    if (num <= 0) {
+        printf("Please enter a positive number.\n");
+        return 1;
+    }
     for(int i = 1; i <= num; i++){
         printf("[%d] Hello world\n", i);
     }
