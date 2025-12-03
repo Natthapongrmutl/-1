e46bd1d15864bd293880755371deee76903a3922
Natthapongrmutl <nooknattha.a@gmail.com>
2025-12-03 13:54:31 +0700

    Update lab1main1.c

--- lab1main1.c
+++ lab1main1.c
@@ -3,19 +3,19 @@
 int main() {
     int num;
 
-    printf("ใส่เลข: ");
+    printf("ใส่เลข: ");    // รับค่าจำนวนรอบจากผู้ใช้
     if (scanf("%d", &num) != 1) {
         printf("Invalid input.\n");
         return 1;
     }
 
     if (num <= 0) {
-        printf("ใส่เลขดีๆ\n");
+        printf("ใส่เลขดีๆ\n");// ตรวจสอบว่าค่าที่กรอกเป็นบวก
         return 1;
     }
 
     for (int i = 1; i <= num; i++) {
-        printf("[%d] Hello world\n", i);
+        printf("[%d] Hello world\n", i);// วนลูปแสดงผล
     }
 
     return 0;
