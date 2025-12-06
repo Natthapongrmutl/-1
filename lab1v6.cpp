53adfc320af30ecbfabe967b2c9a698ff46a76ea
Natthapongrmutl <nooknattha.a@gmail.com>
2025-12-06 20:27:04 +0700

    Update lab1v6.cpp

--- lab1v6.cpp
+++ lab1v6.cpp
@@ -1,32 +1,39 @@
 #include <stdio.h>
 
+// ฟังก์ชันสำหรับสร้างพื้นที่เก็บเมทริกและคืนค่า pointer
 int *GetMatrix(int *rows, int *cols);
 
 int main(){
-    int *data;
-    int m , n;
+    int *data;  // pointer สำหรับเก็บข้อมูลเมทริกซ์
+    int totalRows;  // จำนวนแถว
+    int totalCols;  // จำนวนคอลัมน์
 
-    data = GetMatrix(&m, &n);
+    // เรียกฟังก์ชันเพื่อรับขนาดเมทริกซ์และจองหน่วยความจำ
+    data = GetMatrix(&totalRows, &totalCols);
 
+    // รับค่าของเมทริกซ์จากผู้ใช้
     printf("\nMatrix Input\n");
-    for(int i = 0; i < m; i++){
+    for(int i = 0; i < totalRows; i++){
         printf("%d row\n",i+1);
-        for(int j = 0; j < n; j++){
-            printf("%d col number: ",j+1);
-            scanf("%d", &data[ i * n + j]);
+        for(int colIndex = 0; colIndex < totalCols; colIndex++){
+            printf("%d col number: ",colIndex+1);
+            scanf("%d", &data[ i * totalCols + colIndex]);
         }
     }
 
     return 0;
 }
 
-int *GetMatrix(int *row, int *col){
-    int *var;
+// ------------------------- ฟังก์ชัน -------------------------
+int *GetMatrix(int *numRows, int *numCols){
+    int *varlueMatrix;  // ตัวแปรชั่วคราวสำหรับเก็บข้อมูลเมทริกซ์
 
+    // รับจำนวนแถวและคอลัมน์จากผู้ใช้
     printf("enter rows, cols :");
-    scanf("%d %d", row, col);
+    scanf("%d %d", numRows, numCols);
 
-    var = new int[ *row * *col];
-    
-    return var;
+    // จองหน่วยความจำสำหรับเมทริกซ์แบบ 1 มิติ
+    varlueMatrix = new int[ *numRows * *numCols];
+
+    return varlueMatrix;
 }
