654741cd7c667335e4d0944b55447046b9e197c0
Natthapongrmutl <nooknattha.a@gmail.com>
2025-12-06 13:15:37 +0700

    Update lab1v4.c

--- lab1v4.c
+++ lab1v4.c
@@ -2,18 +2,16 @@
 
 int GetSet(int arr[]){
     int n;
-    
+
         printf("enter number: ");
         scanf("%d",&n);
 
     for (int i = 0; i < n; i++)
     {
-        printf("enter element: ");
+        printf("enter element %d : ",i + 1);
         scanf("%d", &arr[i]);
     }
-
     return n;
-    
 }
 
 int main(){
@@ -21,12 +19,10 @@ int main(){
     int num;
 
     num = GetSet(data);
-
+    printf("\nYour number: \n");
     for (int i = 0; i < num; i++)
     {
         printf("%d ", data[i]);
     }
-
     return 0;
-    
 }
