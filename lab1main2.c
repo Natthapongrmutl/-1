4b224b8d25966301e300f73b77957468c6c34183
Natthapongrmutl <nooknattha.a@gmail.com>
2025-12-12 16:23:50 +0700

    Update lab1main2.c

--- lab1main2.c
+++ lab1main2.c
@@ -2,13 +2,11 @@
 
 int main() {
     int score;
-
-    if (scanf("%d", &score) != 1) {
-
-        return 1; 
-    }
-
-    if (score >= 80 && score <= 100) {
+    
+    printf("Enter your score: ");
+    scanf("%d", &score);
+    
+    if (score >= 80) {
         printf("Grade A\n");
     } else if (score >= 70) {
         printf("Grade B\n");
@@ -16,11 +14,9 @@ int main() {
         printf("Grade C\n");
     } else if (score >= 50) {
         printf("Grade D\n");
-    } else if (score >= 0) {
-        printf("Grade F\n");
     } else {
-        printf("คะแนนไม่ถูกต้อง!\n");
+        printf("Grade F\n");
     }
-
+    
     return 0;
 }
