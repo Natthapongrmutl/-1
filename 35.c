514c0071afaadd6bdcdd98f616c64fc6eaf9c09c
Natthapongrmutl <nooknattha.a@gmail.com>
2025-12-27 19:45:45 +0700

    Update 35.c

--- 35.c
+++ 35.c
@@ -9,28 +9,31 @@ struct Order {
 };
 
 int main() {
+
     int N, i;
     float grandTotal = 0.0;
 
-    // รับจำนวนคำสั่งซื้อ
-    if (scanf("%d", &N) != 1) {
+    if (scanf("%d", &N) != 1 || N <= 0) {
         return 1;
     }
 
     struct Order orders[N];
 
-    // รับข้อมูลคำสั่งซื้อและคำนวณราคารวม
     for (i = 0; i < N; i++) {
+
         float itemTotal;
 
-        if (scanf("%d %f %d", &orders[i].itemId, &orders[i].unitPrice, &orders[i].quantity) != 3) {
+        if (scanf("%d %f %d",
+                  &orders[i].itemId,
+                  &orders[i].unitPrice,
+                  &orders[i].quantity) != 3) {
             return 1;
         }
 
         itemTotal = (orders[i].unitPrice * orders[i].quantity) + SHIPPING_FEE;
 
         if (itemTotal >= 500.0) {
-            itemTotal *= 0.90; // ลด 10%
+            itemTotal = itemTotal * 0.90;
         }
 
         grandTotal += itemTotal;
